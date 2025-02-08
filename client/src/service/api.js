14c5a4a1281702a9786ed7b8534fce35e3fe2769
Md. Sabbir Hossain Onik <105641639+sabbirkhanoni@users.noreply.github.com>
2025-02-08 15:23:48 +0600

    serversetup

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const URL = 'http://localhost:8000';
+const URL = 'https://crud-application-using-mern-server.onrender.com';
 
 export const addUser = async(data) => {
     try {
