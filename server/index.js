d215732229e87ec80fc3285950a08a21cf6257d5
sabbirkhanoni <mdsabbirkhanoni90@gmail.com>
2025-02-08 15:01:22 +0600

    sec-commit

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(cors());
 //rouets
 app.use('/',Routes);
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
